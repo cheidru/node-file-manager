a691b240426840949f18f8f9e904760da0cf37dd
Igor Checheurov <chei@yandex.ru>
2024-10-11 09:59:30 +0300

    feat: add interface for command-line input

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,13 +1,39 @@
-import path from 'path';
+import path from 'node:path';
+import os from 'node:os';
+import { fmCommandLauncher } from 'fmLauncher';
+import readline from 'node:readline/promises';
+import process from 'node:process';
 
 const fileManager = () => {
     const arr = process.argv.slice(2);
     const userName = arr[0].split('=')[1];
     console.log(`Welcome to the File Manager, ${userName}!`);
-    // console.log(`Welcome to the File Manager, ${userName}!`);
-    const workPath = path.resolve('fileManager.js');
-    const workDir = path.dirname(workPath);
-    console.log(`You are currently in ${workDir}\\`);
+
+    let fileManagerExit = false;
+
+    const workDir = os.homedir();
+    process.chdir(workDir);
+
+    const prompt = async(promptMSG) => {
+        const inputInterface = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          });
+        
+        const userInput = await inputInterface.question(promptMSG);
+        inputInterface.close(); // close prompt
+        return userInput;
+    }
+
+    do {
+        console.log(`You are currently in ${workDir}\\`);
+        const commandCode = prompt('Enter your command here: ');
+        fmCommandLauncher(commandCode);
+     } while(!fileManagerExit)
+
+    // const workPath = path.resolve('fileManager.js');
+    // const workDir = path.dirname(workPath);
+
 }
 
 fileManager();
